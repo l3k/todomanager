27f7b424bccfb44353c2168812b439dc954db574
Lucas Krul <l3k.scorp@gmail.com>
2019-05-14 00:14:32 -0300

    first commit, add structure and style

---
+++ src/components/Task.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <v-card class="mt-3"  :class="stateClass">  
+            <v-card-title primary-title>
+                <div style="display: flex">
+                    <h3 class="">{{ task.name }}</h3>
+                </div>
+            </v-card-title>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                    <v-icon>delete</v-icon>
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        task: {
+            type: Object,
+            required: true 
+        },
+    },
+
+    computed: {
+        stateClass() {
+            return {
+                pending: this.task.pending,
+                done: !this.task.pending,
+            }
+        }
+    },
+}
+</script>
+
+<style>
+    .pending {
+        background-color: #C62828 !important;
+        border-color: #C62828 !important;
+    }
+    
+    .done{
+        background-color: #2e7d32 !important;
+        border-color: #2e7d32 !important;
+    }
+</style>
---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <template>
+            <v-layout justify-center>
+                <v-flex xs2>
+                    <task v-for="task in tasks" :key="task.name" :task=task></task>
+                </v-flex>
+            </v-layout>
+        </template>
+    </div>
+</template>
+
+<script>
+    import Task from './Task'
+    export default {
+        components: {
+            Task,
+        },
+        props: {
+            tasks: {
+                type: Array,
+                required: true
+            },
+        },
+        data() {
+            return {
+
+            }
+        }
+    };
+</script>
+
+<style>
+</style>
